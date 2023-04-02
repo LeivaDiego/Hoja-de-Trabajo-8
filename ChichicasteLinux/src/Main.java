fd32b7abd3950f8402e97459a6b7c5f03c78bd00
JPOrellana <orellanaorellanapablo@gmail.com>
2023-04-02 00:14:08 -0600

    Version 6.0 Main
    Implementacion del metodo main para la ejecucion del Programa

--- ChichicasteLinux/src/Main.java
+++ ChichicasteLinux/src/Main.java
@@ -1,5 +1,26 @@
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String archivo = "procesos.txt";
+        List<Process> processes;
+
+        try {
+            processes = ProcessReader.leerProcesos(archivo);
+        } catch (IOException e) {
+            View.Error("No se pudo leer el archivo de procesos: " + e.getMessage());
+            return;
+        }
+        View.Menu();
+        VectorHeap<Process> colaDePrioridad = new VectorHeap<>();
+        for (Process process : processes) {
+            colaDePrioridad.insert(process);
+        }
+
+        while (!colaDePrioridad.isEmpty()) {
+            Process siguienteProceso = colaDePrioridad.remove();
+            View.ShowMessage(siguienteProceso.toString());
+        }
     }
 }
